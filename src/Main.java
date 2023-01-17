ed1b37e05bc5dc19baec7226b83727f1335a328a
cybernico14 <nikissue@gmail.com>
2023-01-17 16:32:29 +0100

    this is the first commit

---
+++ src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+        int numbers[] = {47,75,14,2,97};
+
+        //assign first element of an array to largest and smallest
+        int smallest = numbers[0];
+        int largest = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > largest)
+                largest = numbers[i];
+            else if (numbers[i] < smallest)
+                smallest = numbers[i];
+        }
+
+        System.out.println("Largest Number is : " + largest);
+        System.out.println("Smallest Number is : " + smallest);
+
+
+
+        int n = numbers.length;
+        int smallest1 = Integer.MAX_VALUE;
+        // traversing the array to find
+        // smallest element.
+        for (int i = 0; i < n; i++) {
+            if (numbers[i] < smallest1) {
+                smallest1 = numbers[i];
+            }
+        }
+        System.out.println("smallest element is: "
+                + smallest1);
+
+        int second_smallest = Integer.MAX_VALUE;
+
+        // traversing the array to find second smallest
+        // element
+        for (int i = 0; i < n; i++) {
+            if (numbers[i] < second_smallest
+                    && numbers[i] > smallest) {
+                second_smallest = numbers[i];
+            }
+        }
+        System.out.println("second smallest element is: "
+                + second_smallest);
+    }
+}
+
+
+
+
